d4e10f5b4722c0ee73e9d0a505c137bec322cf19
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2017-10-21 19:54:20 +0200

    Cleanup code

--- api/rules.go
+++ api/rules.go
@@ -3,8 +3,8 @@ package api
 // repositories is a static list of binstack Rules
 // TODO: make it a database and create a file format
 var repositories = map[string]map[string]Rule{
-	"Masterminds/glide": map[string]Rule{
-		"*": Rule{
+	"Masterminds/glide": {
+		"*": {
 			Homepage:     "https://glide.sh",
 			Description:  "Package Management for Golang",
 			UrlTemplate:  "v{{.Version}}/glide-v{{.Version}}-{{.Os}}-{{.Arch}}.tar.gz",
@@ -12,16 +12,16 @@ var repositories = map[string]map[string]Rule{
 			PathTemplate: "{{.Os}}-{{.Arch}}/glide",
 		},
 	},
-	"mattes/migrate": map[string]Rule{
-		"*": Rule{
+	"mattes/migrate": {
+		"*": {
 			Description:  " Database migrations. CLI and Golang library.",
 			UrlTemplate:  "v{{.Version}}/migrate.{{.Os}}-{{.Arch}}.tar.gz",
 			Format:       2,                             // binstack.DownloadInfo_TARGZ
 			PathTemplate: "./migrate.{{.Os}}-{{.Arch}}", // TODO: do not require ./ at the beggining of the path???
 		},
 	},
-	"goreleaser/goreleaser": map[string]Rule{
-		"*": Rule{
+	"goreleaser/goreleaser": {
+		"*": {
 			Homepage:     "https://goreleaser.github.io/",
 			Description:  "Deliver Go binaries as fast and easily as possible",
 			UrlTemplate:  "v{{.Version}}/goreleaser_{{.Os | title}}_{{.Arch | archReplace}}.tar.gz",
@@ -29,14 +29,14 @@ var repositories = map[string]map[string]Rule{
 			PathTemplate: "goreleaser",
 		},
 	},
-	"golang/dep": map[string]Rule{
-		">0.3.0": Rule{
+	"golang/dep": {
+		">0.3.0": {
 			Homepage:    "https://github.com/golang/dep",
 			Description: "Go dependency management tool",
 			UrlTemplate: "v{{.Version}}/dep-{{.Os}}-{{.Arch}}",
 			Format:      1, // binstack.DownloadInfo_BINARY
 		},
-		"<=0.3.0": Rule{
+		"<=0.3.0": {
 			Homepage:     "https://github.com/golang/dep",
 			Description:  "Go dependency management tool",
 			UrlTemplate:  "v{{.Version}}/dep-{{.Os}}-{{.Arch}}.zip",
